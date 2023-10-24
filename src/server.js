2cd0250df6a03ddb5e951b3c1162407295044855
HeroViet <supertiger159@gmail.com>
2023-10-25 00:00:10 +0700

    update MVC p1

--- src/server.js
+++ src/server.js
@@ -1,25 +1,17 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
-require("dotenv").config();
+const configViewEngine = require("./config/viewEngine");
+const webRoutes = require("./routes/web");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 //config template engine
-app.set("views", path.join(__dirname, "views"));
-app.set("views engine", "ejs");
-
-//config static files
-app.use(express.static(path.join(__dirname, "public")));
+configViewEngine(app);
 
 //Khai bao route
-app.get("/", (req, res) => {
-  res.send("Hello World!!");
-});
-
-app.get("/abc", (req, res) => {
-  res.render("sample.ejs");
-});
+app.use("/", webRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
